b9fa47b95413c4137b712c248f565d497065e4d2
raysantori <contato@raysantori.com>
2023-03-19 12:53:36 -0300

    fix footer js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,4 @@
-var footer = window.document.querySelector('#copyright')
+var footer = window.document.querySelector('.footer')
 var currentDate = new Date()
 var currentYear = currentDate.getFullYear()
 footer.innerHTML = `&copy; ${currentYear} raysantori`
